5f5227ec4bc3ed271f0bf7a43d20e70105298868
steven-frasica <steven.frasica@gmail.com>
2026-09-08 17:33:48 -0400

    edit handleLoadMore function so only 16 cards load

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Countdown from "../UI/Countdown";
 
-const ExploreItems = ({ exploreItems }) => {
+const ExploreItems = ({ exploreItems, loading, error }) => {
+  const [visibleCount, setVisibleCount] = useState(8);
+
+  const handleLoadMore = () => {
+    setVisibleCount((currentCount) =>
+      Math.min(currentCount + 4, exploreItems.length),
+    );
+  };
+
   return (
     <>
       <div>
@@ -13,65 +21,90 @@ const ExploreItems = ({ exploreItems }) => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {exploreItems.map(({ id, authorId, authorImage, nftImage, nftId, title, price, likes, expiryDate }) => (
-        <div
-          key={id}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to={`/author/${authorId}`}
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={authorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <Countdown expiration={expiryDate} />
+      {exploreItems
+        .slice(0, visibleCount)
+        .map(
+          ({
+            id,
+            authorId,
+            authorImage,
+            nftImage,
+            nftId,
+            title,
+            price,
+            likes,
+            expiryDate,
+          }) => (
+            <div
+              key={id}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <div className="nft__item">
+                <div className="author_list_pp">
+                  <Link
+                    to={`/author/${authorId}`}
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                  >
+                    <img className="lazy" src={authorImage} alt="" />
+                    <i className="fa fa-check"></i>
+                  </Link>
+                </div>
+                <Countdown expiration={expiryDate} />
 
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
+                <div className="nft__item_wrap">
+                  <div className="nft__item_extra">
+                    <div className="nft__item_buttons">
+                      <button>Buy Now</button>
+                      <div className="nft__item_share">
+                        <h4>Share</h4>
+                        <a href="" target="_blank" rel="noreferrer">
+                          <i className="fa fa-facebook fa-lg"></i>
+                        </a>
+                        <a href="" target="_blank" rel="noreferrer">
+                          <i className="fa fa-twitter fa-lg"></i>
+                        </a>
+                        <a href="">
+                          <i className="fa fa-envelope fa-lg"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <Link to={`/item-details/${nftId}`}>
+                    <img
+                      src={nftImage}
+                      className="lazy nft__item_preview"
+                      alt=""
+                    />
+                  </Link>
+                </div>
+                <div className="nft__item_info">
+                  <Link to={`/item-details/${nftId}`}>
+                    <h4>{title}</h4>
+                  </Link>
+                  <div className="nft__item_price">{price} ETH</div>
+                  <div className="nft__item_like">
+                    <i className="fa fa-heart"></i>
+                    <span>{likes}</span>
                   </div>
                 </div>
               </div>
-              <Link to={`/item-details/${nftId}`}>
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
             </div>
-            <div className="nft__item_info">
-              <Link to={`/item-details/${nftId}`}>
-                <h4>{title}</h4>
-              </Link>
-              <div className="nft__item_price">{price} ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>{likes}</span>
-              </div>
-            </div>
-          </div>
+          ),
+        )}
+      {visibleCount < exploreItems.length && (
+        <div className="col-md-12 text-center">
+          <button
+            onClick={handleLoadMore}
+            id="loadmore"
+            className="btn-main lead"
+            type="button"
+          >
+            Load more
+          </button>
         </div>
-      ))}
-      <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
-      </div>
+      )}
     </>
   );
 };
