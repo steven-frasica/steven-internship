23f07173ed8c07307cce77a49f844e55422c53c6
steven-frasica <steven.frasica@gmail.com>
2026-09-13 11:51:37 -0400

    Make author page dynamic and add follow button functionality

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 
-const AuthorItems = () => {
+const AuthorItems = ({ nftCollection, authorId, authorImage }) => {
+  
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {new Array(8).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {nftCollection.map(({ id, nftImage, nftId, title, price, likes }) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={id}>
               <div className="nft__item">
                 <div className="author_list_pp">
-                  <Link to="">
-                    <img className="lazy" src={AuthorImage} alt="" />
+                  <Link to={`/author/${authorId}`}>
+                    <img className="lazy" src={authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
@@ -35,7 +34,7 @@ const AuthorItems = () => {
                       </div>
                     </div>
                   </div>
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${nftId}`}>
                     <img
                       src={nftImage}
                       className="lazy nft__item_preview"
@@ -44,13 +43,13 @@ const AuthorItems = () => {
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                  <Link to={`/item-details/${nftId}`}>
+                    <h4>{title}</h4>
                   </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
+                  <div className="nft__item_price">{price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>97</span>
+                    <span>{likes}</span>
                   </div>
                 </div>
               </div>
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,24 +1,23 @@
 import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link, useParams } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 
 const Author = () => {
   const [author, setAuthor] = useState(null);
-  console.log(author);
+  const [following, setFollowing] = useState(false);
   // useParams() gets the authorId from the Route in App.jsx
   const { authorId } = useParams();
 
   useEffect(() => {
     async function getAuthorInfo() {
+      setFollowing(false)
       // use authorId from useParams in the fetch request
       const { data } = await axios.get(
         `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`,
       );
       setAuthor(data);
-      // console.log(data)
     }
     // Call async fn in useEffect
     getAuthorInfo();
@@ -48,7 +47,7 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      <img src={author.authorImage} alt="" />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
@@ -56,7 +55,7 @@ const Author = () => {
                           {author.authorName}
                           <span className="profile_username">@{author.tag}</span>
                           <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            {author.address}
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
@@ -67,10 +66,10 @@ const Author = () => {
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      <div className="profile_follower">{following ? author.followers + 1 : author.followers} followers</div>
+                      <button type="button" className="btn-main" onClick={() => setFollowing(!following)}>
+                        {following ? "Unfollow" : "Follow"}
+                      </button>
                     </div>
                   </div>
                 </div>
@@ -78,7 +77,7 @@ const Author = () => {
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems nftCollection={author.nftCollection} authorId={author.authorId} authorImage={author.authorImage} />
                 </div>
               </div>
             </div>
