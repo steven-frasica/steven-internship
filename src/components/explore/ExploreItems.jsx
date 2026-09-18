9894868041207aeb232f93f6435abed85a7eeffb
steven-frasica <steven.frasica@gmail.com>
2026-09-18 00:38:27 -0400

    Implement reusable card in Explore page

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import Countdown from "../UI/Countdown";
 import Skeleton from "../UI/Skeleton";
+import NftItemCard from "../UI/NftItemCard";
 
 const ExploreItems = ({ exploreItems, loading, error, sortBy, setSortBy }) => {
   const [visibleCount, setVisibleCount] = useState(8);
@@ -80,56 +79,17 @@ const ExploreItems = ({ exploreItems, loading, error, sortBy, setSortBy }) => {
                 className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
                 style={{ display: "block", backgroundSize: "cover" }}
               >
-                <div className="nft__item">
-                  <div className="author_list_pp">
-                    <Link
-                      to={`/author/${authorId}`}
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                    >
-                      <img className="lazy" src={authorImage} alt="" />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <Countdown expiration={expiryDate} />
-
-                  <div className="nft__item_wrap">
-                    <div className="nft__item_extra">
-                      <div className="nft__item_buttons">
-                        <button>Buy Now</button>
-                        <div className="nft__item_share">
-                          <h4>Share</h4>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-facebook fa-lg"></i>
-                          </a>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-twitter fa-lg"></i>
-                          </a>
-                          <a href="">
-                            <i className="fa fa-envelope fa-lg"></i>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                    <Link to={`/item-details/${nftId}`}>
-                      <img
-                        src={nftImage}
-                        className="lazy nft__item_preview"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div className="nft__item_info">
-                    <Link to={`/item-details/${nftId}`}>
-                      <h4>{title}</h4>
-                    </Link>
-                    <div className="nft__item_price">{price} ETH</div>
-                    <div className="nft__item_like">
-                      <i className="fa fa-heart"></i>
-                      <span>{likes}</span>
-                    </div>
-                  </div>
-                </div>
+                <NftItemCard 
+                  authorId={authorId}
+                  authorImage={authorImage}
+                  nftId={nftId}
+                  nftImage={nftImage}
+                  title={title}
+                  price={price}
+                  likes={likes}
+                  expiryDate={expiryDate}
+                  showCountdown={true}
+                />
               </div>
             ),
           )
